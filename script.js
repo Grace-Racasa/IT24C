5d7ec1218c28316de9c855747482ed9d9524c753
Grace-Racasa <racasa.grace1@gmail.com>
2025-09-09 15:47:08 +0800

    Added Get Elements

--- script.js
+++ script.js
@@ -1,13 +1,7 @@
-// document.getElementById("btn").addEventListener ("Click", function(){
-//     alert("You Clicked Me!");
-// });
+const nameInput = document.getElementById("nameInput");
+const nameOutput = document.getElementById("nameOutput");
+const quoteBtn = document.getElementById("quoteBtn");
+const quoteOutput = document.getElementById("quoteOutput");
 
+const quotes = ["Be yourself; everyone else is already taken.― Oscar Wilde", "“I'm selfish, impatient and a little insecure. I make mistakes, I am out of control and at times hard to handle. But if you can't handle me at my worst, then you sure as hell don't deserve me at my best.”― Marilyn Monroe", "So many books, so little time.― Frank Zappa", "“Two things are infinite: the universe and human stupidity; and I'm not sure about the universe.”", "A room without books is like a body without a soul."];
 
-function updateDisplay(){
-   let Textbox = document.getElementById("Textbox");
-   let displaytext = document.getElementById("display");
-
-    displaytext.textContent = Textbox.value;
-
-
-}
