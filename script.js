f885f925f903c92a18fb72d24275a77397b3caa8
Grace-Racasa <racasa.grace1@gmail.com>
2025-09-09 15:50:27 +0800

    Added input event

--- script.js
+++ script.js
@@ -5,3 +5,10 @@ const quoteOutput = document.getElementById("quoteOutput");
 
 const quotes = ["Be yourself; everyone else is already taken.― Oscar Wilde", "“I'm selfish, impatient and a little insecure. I make mistakes, I am out of control and at times hard to handle. But if you can't handle me at my worst, then you sure as hell don't deserve me at my best.”― Marilyn Monroe", "So many books, so little time.― Frank Zappa", "“Two things are infinite: the universe and human stupidity; and I'm not sure about the universe.”", "A room without books is like a body without a soul."];
 
+nameInput.addEventListener("input", () => {
+    nameOutput.textContent = 'hello {nameInput.value}!';
+});
+quoteBtn.addEventListener("click", () => {
+    let randomIndex = Math.floor(Math.random() * quotes.length);
+    quoteOutput.textContent = quotes[randomIndex];
+});
